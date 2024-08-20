6ffc82184aba3f45c5152d98f7dccb7766fbbbd8
NightmareJahid <jahiduljeesan@gmail.com>
2024-08-21 00:00:10 +0600

    dfd

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         actMain = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(actMain.getRoot());
 
+
         actMain.btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
